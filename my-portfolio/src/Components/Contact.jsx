f3e5b631ca08fb5d01a93ebb4a8a39e6a187e8a7
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-09-04 22:30:49 +0545

    Contact Page Created with code correction

--- my-portfolio/src/Components/Contact.jsx
+++ my-portfolio/src/Components/Contact.jsx
@@ -51,8 +51,8 @@ function Contact() {
       className="relative bg-[#F8FAFC] text-[#0F172A] py-14 overflow-hidden"
     >
       {/* Background Decoration */}
-      <div className="absolute top-20 right-0 w-96 h-96 bg-purple-200/30 rounded-full blur-3xl -z-0" />
-      <div className="absolute bottom-0 left-0 w-72 h-72 bg-violet-100/50 rounded-full blur-3xl -z-0" />
+      <div className="absolute top-20 right-0 w-96 h-96 bg-purple-200/30 rounded-full blur-3xl z-0" />
+      <div className="absolute bottom-0 left-0 w-72 h-72 bg-violet-100/50 rounded-full blur-3xl z-0" />
 
       <div className="relative z-10 max-w-7xl mx-auto px-6 lg:px-8">
         {/* Heading */}
